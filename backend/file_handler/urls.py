af8b255eb95300ca1b2088869d7cb9aaa1cfb908
simonmayer <simon.mayer@rigi.tech>
2024-01-09 17:35:32 +0100

    minor changes

--- backend/file_handler/urls.py
+++ backend/file_handler/urls.py
@@ -3,5 +3,4 @@ from .views import FileUploadView
 
 urlpatterns = [
     path("file_handler/", FileUploadView.as_view(), name="file_handler"),
-    # Define more URL patterns for other endpoints if needed
 ]
