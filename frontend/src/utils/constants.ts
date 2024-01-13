e8107f9fb95cdc13ecbf74728594070dbc08ca2b
xoxkoo <anton.durcak22@gmail.com>
2024-01-13 16:07:01 +0100

    endpoint url fix

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { ConversationAuthor } from '../model';
 
-export const ENDPOINT_URL = 'http://localhost:8000/api/';
+export const ENDPOINT_URL = 'http://localhost/api/api/';
 export const TOAST_LIFE = 7000; // after 7 sec the alert will hide
 
 export const DEFAULT_MESSAGE = [{ author: ConversationAuthor.GPT, message: 'message' }];
